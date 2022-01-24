7a7aa7630188819c6d50e4ec1bde4f842de4cc0f
hy299792458@gmail.com <hy299792458@gmail.com>
2022-01-24 00:30:23 -0800

    lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
 
 @Module({
   imports: [
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(new ValidationPipe())
 
-  await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  await app.listen(3000)
+  console.log(`Application is running on: ${await app.getUrl()}`)
 }
-bootstrap();
+
+bootstrap()
